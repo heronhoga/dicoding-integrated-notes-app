c7ddc8480bd74a8a0b49d1a062089a3531c2af2f
heronhoga <nozzeravixer@gmail.com>
2025-05-21 10:37:39 +0700

    feat:integrate create note function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import "./style.css";
 
 const notesContainer = document.getElementById("notes-container");
 
+function createNoteElement(id, title, body, createdAt, archived) {
+  const note = document.createElement("note-item");
+  note.setAttribute("id", id);
+  note.setAttribute("title", title);
+  note.setAttribute("body", body);
+  note.setAttribute("createdat", createdAt);
+  note.setAttribute("archived", archived);
+  notesContainer.appendChild(note);
+}
+
 async function getNotes() {
   const url = "https://notes-api.dicoding.dev/v2/notes";
   try {
@@ -21,6 +31,33 @@ async function getNotes() {
   }
 }
 
+function createNote(data) {
+  const url = "https://notes-api.dicoding.dev/v2/notes";
+  console.log(data);
+
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((notes) => {
+      const notesData = notes.data;
+      console.log(notesData);
+      return notesData;
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+}
+
 const notesData = await getNotes();
 
 notesData.forEach((note) => {
@@ -33,15 +70,25 @@ notesData.forEach((note) => {
   );
 });
 
-document.addEventListener("note-submitted", (e) => {
+document.addEventListener("note-submitted", async (e) => {
   const { title, body } = e.detail;
+  const dataInput = { title, body };
+
+  const newData = await createNote(dataInput);
+
+  if (!newData) {
+    console.error("Failed to create note.");
+    return;
+  }
+
   const newNote = {
-    id: `notes-${Math.random().toString(36).substring(2, 10)}`,
-    title,
-    body,
-    createdAt: new Date().toISOString(),
-    archived: false,
+    id: newData.id,
+    title: newData.title,
+    body: newData.body,
+    createdAt: new Date(newData.createdAt).toISOString(),
+    archived: newData.archived,
   };
+
   createNoteElement(
     newNote.id,
     newNote.title,
@@ -51,12 +98,3 @@ document.addEventListener("note-submitted", (e) => {
   );
 });
 
-function createNoteElement(id, title, body, createdAt, archived) {
-  const note = document.createElement("note-item");
-  note.setAttribute("id", id);
-  note.setAttribute("title", title);
-  note.setAttribute("body", body);
-  note.setAttribute("createdat", createdAt);
-  note.setAttribute("archived", archived);
-  notesContainer.appendChild(note);
-}
